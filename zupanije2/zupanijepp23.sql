bc7af4ae9abb63e03af76ddf61d6b02de0f89112
Antonia Čičin-Ćapin <antonia.cicincapin@gmail.com>
2021-05-15 19:18:36 +0200

    insert

--- zupanije2/zupanijepp23.sql
+++ zupanije2/zupanijepp23.sql
@@ -3,25 +3,25 @@ create database zupanijepp23;
 use zupanijepp23;
 
 create table zupanija (
-sifra int not null primary key auto_increment,
+sifra int (11) not null primary key auto_increment,
 naziv varchar (50),
 zupan int not null
 );
 
 create table zupan (
-sifra int not null primary key auto_increment,
+sifra int (11) not null primary key auto_increment,
 ime varchar (50),
 prezime varchar(50)
 );
 
 create table opcina(
-sifra int not null primary key auto_increment,
-zupanija int,
+sifra int (11) not null primary key auto_increment,
+zupanija int (11),
 naziv varchar (50)
 );
 
 create table mjesto(
-sifra int not null primary key auto_increment,
+sifra int (11) not null primary key auto_increment,
 opcina int,
 naziv varchar (50)
 );
@@ -31,5 +31,51 @@ alter table zupanija add foreign key (zupan) references zupan (sifra);
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 
 alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+
+insert into zupan (ime,prezime) values 
+('pero','peric'),
+('marko','markovic'),
+('janko','jankovic');
+
+select * from zupan;
+
+insert into zupanija (zupan,naziv) values
+(1,'osijecko-baranjska'),
+(2,'zagrebacka'),
+(3,'zadarska');
+
+select * from zupanija;
+
+insert into opcina (naziv) values
+('bilje'),
+('ernestinovo'),
+('dugo selo'),
+('samobor'),
+('vir'),
+('nin');
+
+select * from opcina;
+
+insert into mjesto (naziv) values
+('osijek'),
+('nasice'),
+('valpovo'),
+('zagreb'),
+('velika gorica'),
+('jastrebarsko'),
+('sukosan'),
+('zadar'),
+('bibinje');
+
+select * from mjesto;
+
+
+
+
+
+
+
+
  
 
